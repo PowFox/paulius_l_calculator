035119438bbc0e512129575b94c627b952b1492e
ErnestasZ <ernestas.zemaitis@gmail.com>
2024-10-07 14:52:03 +0300

    new commit

--- main.py
+++ main.py
@@ -2,18 +2,21 @@ class Calculator:
     def __init__(self, symbol: str, number: float) -> None:
         self.symbol = symbol
         self.number = number
-    
-    def add(self) -> float:
-        ...
 
-    def sub(self) -> float:
-        ...
+    def add(self, new_number: int) -> float:
+        return self.number + new_number
 
-    def div(self) -> float:
-        ...
+    def sub(self, new_number) -> float:
+        return self.number + new_number
+
+    def div(self, new_number) -> float:
+        try:
+            return self.number / new_number
+        except Exception as err:
+            print(f"Klaida: {err}")
 
     def mul(self) -> float:
         ...
-        
+
     def calculate(self) -> float:
         ...
