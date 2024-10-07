07189e1a9af07f1fe1e34c930a2f05fb38025146
centr <centras51@gmail.com>
2024-10-07 14:54:19 +0300

    supildytas kodas

--- main.py
+++ main.py
@@ -2,18 +2,31 @@ class Calculator:
     def __init__(self, symbol: str, number: float) -> None:
         self.symbol = symbol
         self.number = number
-    
+
     def add(self) -> float:
-        ...
+        return self.number + self.number
 
     def sub(self) -> float:
-        ...
+        return self.number - 15
 
     def div(self) -> float:
-        ...
+        return self.number / 2
 
     def mul(self) -> float:
-        ...
-        
+        return self.number * self.number
+
     def calculate(self) -> float:
-        ...
+        if self.symbol == "+":
+            return self.add()
+        elif self.symbol == "-":
+            return self.sub()
+        elif self.symbol == "*":
+            return self.mul()
+        elif self.symbol == "/":
+            return self.div()
+        
+        
+calculator = Calculator("+", 12)
+
+result = calculator.calculate()
+print(result)
